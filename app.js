750ec2ddbe663f77e6d47cfe283954ed806fcd14
Stuart Urback <stuart@urback.net>
2016-02-08 11:22:30 -0700

    Add API route for authors call in view

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var books = require('./routes/books');
 var authors = require('./routes/authors');
+var api = require('./routes/api')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/authors', authors);
 app.use('/books', books);
+app.use('/api', api)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
