b9816d7927e4898f9ea42a015235c9c7083d48d8
Stuart Urback <stuart@urback.net>
2016-02-09 14:25:51 -0700

    Complete create author

--- routes/authors.js
+++ routes/authors.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var db = require('../lib/authors')
+var validator = require('../lib/validations')
+
+
+
 
 /* GET authors listing. */
 router.get('/', function(req, res, next) {
@@ -10,10 +14,28 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.post('/', function(req, res, next){
+  var validated = validator.validateBook(req.body);
+  if(validated.length > 0){
+    res.render('forms/author', {action: '/authors', author: req.body})
+  }else{
+    db.createAuthor(req.body, function(id){
+      res.redirect('/authors/' + id);
+    })
+  }
+})
+
+router.get('/new', function(req, res, next){
+
+  res.render('forms/author', {action: '/authors'})
+})
+
 router.get('/:id', function(req, res, next){
   db.returnOneAuthorWithBooks(req.params.id, function(author, books){
     res.render('authors/one', {author: author, books: books})
   })
 })
+
+
 
 module.exports = router;
