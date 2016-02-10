60aaab97de026d15239e010f6ac89bae1d21e012
Stuart Urback <stuart@urback.net>
2016-02-09 20:51:55 -0700

    Add filters

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,4 +14,32 @@ $('.deleteauthors').on('click', '.delete', function(){
   $(this).remove();
 })
 
+var testarray = $('.allbooks').find('.book').toArray();
+if (testarray.length > 5){
+  testarray.forEach(function(item){
+    $(item).css({"display" : "none"})
+  })
+  var buttons = Math.round((testarray.length - 5) / 5 + 0.5);
+  $('.options').append('<div id="filter"></div>')
+  for (var i = 1; i <=buttons; i++){
+    $('#filter').append('<a id="page-'+i+'">' + i +  '</a>')
+  }
+  testarray.forEach(function(item, i){
+    if ( i < 5){
+      $(item).css({"display" : "flex"})
+    }
+  })
+  $('#filter').on('click', '[id^="page-"]', function(){
+    var filter = $(this).text();
+    filter = +filter;
+    testarray.forEach(function(item, i){
+      if (i > filter * 5 - 5 && i < 5 * filter){
+        $(item).css({"display" : "flex"})
+      }else{
+        $(item).css({"display" : "none"})
+      }
+    })
+  })
+}
+
 })
